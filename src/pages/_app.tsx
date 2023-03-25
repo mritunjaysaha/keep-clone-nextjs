fb7fbfdbfa27b06d873f1917a98dfcd329466f30
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-25 21:28:30 +0530

    feat: added react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 
 import { Layouts } from '@/layouts/Layouts';
 import { store } from '@/redux/store';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient();
+
   return (
     <Provider store={store}>
-      <Layouts>
-        <Component {...pageProps} />
-      </Layouts>
+      <QueryClientProvider client={queryClient}>
+        <Layouts>
+          <Component {...pageProps} />
+        </Layouts>
+      </QueryClientProvider>
     </Provider>
   );
 };

cef296155868b846369e7768a65460e05d77517f
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-04-22 23:40:27 +0530

    feat: updated take a input

--- src/components/TakeANote/TakeANote.tsx
+++ src/components/TakeANote/TakeANote.tsx
@@ -4,36 +4,35 @@ import { useState } from 'react';
 import type { Todo } from '@/types/todos/Todo';
 
 export function TakeANote() {
-  const [todo, setTodo] = useState<Pick<Todo, 'title' | 'body'>>({ title: '', body: '' });
-  const [isActive, setIsActive] = useState(false);
+  const [todo, setTodo] = useState<Pick<Todo, 'title' | 'body'>>({
+    title: '',
+    body: '',
+  });
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const { value, id } = e.target;
-    setTodo({ ...todo, [id]: value });
-  }
-
-  function handleActive() {
-    setIsActive(!isActive);
+    setTodo((prev) => ({ ...prev, [id]: value }));
   }
 
   return (
     <section
-      className={`box-shadow-editor mx-auto my-8 flex ${
-        isActive ? 'h-max' : 'h-12'
-      } w-50vw flex-col justify-center rounded-md bg-inherit p-2`}
-      onBlur={handleActive}
+      className={`box-shadow-editor mx-auto my-8 flex h-max
+       w-50vw flex-col justify-center rounded-md bg-inherit p-2`}
     >
       <form className='w-2/3'>
-        {isActive && (
-          <input id='title' className='w-full bg-inherit' value={todo.title} placeholder='Title' />
-        )}
+        <input
+          id='title'
+          className='w-full bg-inherit'
+          value={todo.title}
+          onChange={handleChange}
+          placeholder='Title'
+        />
         <input
           id='body'
           className='w-full bg-inherit'
           value={todo.body}
           onChange={handleChange}
           placeholder='Take a note...'
-          onClick={handleActive}
         />
       </form>
     </section>

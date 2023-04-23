4e3adb4f374f1cc3136c36816643cb666d9150ed
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-04-23 12:58:50 +0530

    feat: added debouncing for take a note

--- src/components/TakeANote/TakeANote.tsx
+++ src/components/TakeANote/TakeANote.tsx
@@ -1,37 +1,59 @@
-import type { ChangeEvent } from 'react';
-import { useState } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import type { Todo } from '@/types/todos/Todo';
 
-export function TakeANote() {
-  const [todo, setTodo] = useState<Pick<Todo, 'title' | 'body'>>({
-    title: '',
-    body: '',
-  });
+type TodoFormData = Pick<Todo, 'title' | 'body'>;
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const { value, id } = e.target;
-    setTodo((prev) => ({ ...prev, [id]: value }));
+export function TakeANote() {
+  const { register, handleSubmit } = useForm<TodoFormData>();
+
+  const onSubmit: SubmitHandler<TodoFormData> = (data) => {
+    console.log(data);
+  };
+
+  function debounce<T extends (...args: any[]) => void>(
+    func: T,
+    delay: number,
+  ) {
+    let timer: ReturnType<typeof setTimeout> | undefined;
+
+    return function (this: unknown, ...args: Parameters<T>) {
+      const that = this;
+
+      clearTimeout(timer);
+
+      timer = setTimeout(function () {
+        func.apply(that, args);
+      }, delay);
+    };
   }
+
+  const debounceSubmit = debounce(handleSubmit(onSubmit), 500);
 
   return (
     <section
       className={`box-shadow-editor mx-auto my-8 flex h-max
-       w-50vw flex-col justify-center rounded-md bg-inherit p-2`}
+       w-50vw flex-col justify-center rounded-md bg-inherit p-4`}
     >
-      <form className='w-2/3'>
+      <form
+        className='flex w-2/3 flex-col gap-4'
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <input
-          id='title'
-          className='w-full bg-inherit'
-          value={todo.title}
-          onChange={handleChange}
+          {...register('title', {
+            onChange: () => {
+              debounceSubmit();
+            },
+          })}
           placeholder='Title'
         />
         <input
-          id='body'
-          className='w-full bg-inherit'
-          value={todo.body}
-          onChange={handleChange}
+          {...register('body', {
+            onChange: () => {
+              debounceSubmit();
+            },
+          })}
           placeholder='Take a note...'
         />
       </form>

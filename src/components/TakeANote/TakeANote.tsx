9e2296362df2fb7fb9ffd0fa835fccaec5565354
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-04-27 00:35:28 +0530

    feat: added onOutsideClick

--- src/components/TakeANote/TakeANote.tsx
+++ src/components/TakeANote/TakeANote.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { BsPin, BsPinFill } from 'react-icons/bs';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 import { ButtonIcon } from '@/components/Atoms/ButtonIcon/ButtonIcon';
 import { TextArea } from '@/components/Atoms/TextArea/TextArea';
@@ -14,6 +15,8 @@ type TodoFormData = Pick<Todo, 'title' | 'body'>;
 export function TakeANote() {
   const { register, handleSubmit } = useForm<TodoFormData>();
   const [isPinned, setIsPinned] = useState(false);
+
+  const [isClicked, setIsClicked] = useState(false);
 
   const onSubmit: SubmitHandler<TodoFormData> = (data) => {
     console.log(data);
@@ -33,33 +36,50 @@ export function TakeANote() {
       className={`box-shadow-editor mx-auto my-8 flex min-h-0
        w-50vw flex-col justify-center rounded-md bg-inherit p-4`}
     >
-      <form
-        className='flex w-full flex-col gap-4'
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className='flex items-baseline'>
-          <TextArea
-            {...register('title', {
-              onChange: handleTextAreaChange,
-            })}
-            placeholder='Title'
-          />
-          <ButtonIcon
-            icon={isPinned ? BsPinFill : BsPin}
-            size={20}
+      <OutsideClickHandler onOutsideClick={() => setIsClicked(false)}>
+        {!isClicked && (
+          <p
             onClick={() => {
-              setIsPinned(!isPinned);
+              setIsClicked(true);
             }}
-          ></ButtonIcon>
-        </div>{' '}
-        <TextArea
-          {...register('body', {
-            onChange: handleTextAreaChange,
-          })}
-          placeholder='Take a note...'
-          rows={2}
-        />
-      </form>
+            className='px-3 text-slate-500'
+          >
+            Take a note ...
+          </p>
+        )}
+        {isClicked && (
+          <form
+            className='flex w-full flex-col gap-4'
+            onSubmit={handleSubmit(onSubmit)}
+            onClick={() => {
+              setIsClicked(true);
+            }}
+          >
+            <div className='flex items-baseline'>
+              <TextArea
+                {...register('title', {
+                  onChange: handleTextAreaChange,
+                })}
+                placeholder='Title'
+              />
+              <ButtonIcon
+                icon={isPinned ? BsPinFill : BsPin}
+                size={20}
+                onClick={() => {
+                  setIsPinned(!isPinned);
+                }}
+              ></ButtonIcon>
+            </div>{' '}
+            <TextArea
+              {...register('body', {
+                onChange: handleTextAreaChange,
+              })}
+              placeholder='Take a note...'
+              rows={2}
+            />
+          </form>
+        )}{' '}
+      </OutsideClickHandler>
     </section>
   );
 }
